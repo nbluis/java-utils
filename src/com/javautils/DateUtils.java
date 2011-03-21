b2f3f2e7b061948aa0021207549fadcd0e0eeda3
Eduardo Bohrer <nbluisrs@gmail.com>
2011-03-20 21:43:32 -0300

    identation

--- src/com/javautils/DateUtils.java
+++ src/com/javautils/DateUtils.java
@@ -38,30 +38,30 @@ public class DateUtils {
 	 * 	<li>Calendar.MILLISECOND: trunca a data a partir do milisegundo (30/mar/2010 15:27:32.000).</li>
 	 * 	</ul>
 	 * 
-	 * @return
+	 * @return nova data truncada a partir do nivel informado.
 	 */
 	public static Date truncate(Date date, int level) {
 		if (date == null) throw new IllegalArgumentException("invalid date");
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		switch (level) {
-		case Calendar.YEAR:
-			cal.set(Calendar.YEAR, cal.getActualMinimum(Calendar.YEAR));
-		case Calendar.MONTH:
-			cal.set(Calendar.MONTH, cal.getActualMinimum(Calendar.MONTH));
-		case Calendar.DAY_OF_MONTH:
-			cal.set(Calendar.DAY_OF_MONTH, cal.getActualMinimum(Calendar.DAY_OF_MONTH));
-		case Calendar.HOUR_OF_DAY:
-			cal.set(Calendar.HOUR_OF_DAY, cal.getActualMinimum(Calendar.HOUR_OF_DAY));
-		case Calendar.MINUTE:
-			cal.set(Calendar.MINUTE, cal.getActualMinimum(Calendar.MINUTE));
-		case Calendar.SECOND:
-			cal.set(Calendar.SECOND, cal.getActualMinimum(Calendar.SECOND));
-		case Calendar.MILLISECOND:
-			cal.set(Calendar.MILLISECOND, cal.getActualMinimum(Calendar.MILLISECOND));
-			break;
-		default:
-			throw new IllegalArgumentException("invalid level");
+			case Calendar.YEAR:
+				cal.set(Calendar.YEAR, cal.getActualMinimum(Calendar.YEAR));
+			case Calendar.MONTH:
+				cal.set(Calendar.MONTH, cal.getActualMinimum(Calendar.MONTH));
+			case Calendar.DAY_OF_MONTH:
+				cal.set(Calendar.DAY_OF_MONTH, cal.getActualMinimum(Calendar.DAY_OF_MONTH));
+			case Calendar.HOUR_OF_DAY:
+				cal.set(Calendar.HOUR_OF_DAY, cal.getActualMinimum(Calendar.HOUR_OF_DAY));
+			case Calendar.MINUTE:
+				cal.set(Calendar.MINUTE, cal.getActualMinimum(Calendar.MINUTE));
+			case Calendar.SECOND:
+				cal.set(Calendar.SECOND, cal.getActualMinimum(Calendar.SECOND));
+			case Calendar.MILLISECOND:
+				cal.set(Calendar.MILLISECOND, cal.getActualMinimum(Calendar.MILLISECOND));
+				break;
+			default:
+				throw new IllegalArgumentException("invalid level");
 		}
 		return cal.getTime();
 	}
